58721e26352f3a77527d0aef1c04a32c5c8840cb
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-30 17:00:45 -0500

    Process user input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,34 @@
-getUserRepos("microsoft");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+userFormEl.addEventListener("submit", formSubmitHandler);
 
 function getUserRepos(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 }
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username.");
+    }
+}
+
+function displayRepos(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
