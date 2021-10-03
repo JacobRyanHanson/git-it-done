2dfc2678f68261baf375e060fdf0ac1238d59c2f
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-10-03 12:31:29 -0500

    Add displayWarning method

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
-getRepoIssues("octocat/linguist");
+getRepoIssues("octocat/Spoon-Knife");
 
 function getRepoIssues(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,10 @@ function getRepoIssues(repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+               
+                if (response.headers.get("link")) {
+                    displayWarning(repo);  
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -42,4 +47,15 @@ function displayIssues(issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+function displayWarning(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
 }
