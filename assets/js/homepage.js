f47b99092ec49a6631478294509048a6a662d5b9
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-10-03 11:33:58 -0500

    Display issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,20 +8,17 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 function getUserRepos(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    fetch(apiUrl)
-        .then(function (response) {
-            if (response.ok) {
-                response.json()
-                    .then(function (data) {
-                        displayRepos(data, user);
-                    });
-            } else {
-                alert("Error: GitHub User Not Found");
-            }
-        })
-        .catch(function(error) {
-            alert("Unable to connect to GitHub");
-        });
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    }).catch(function (error) {
+        alert("Unable to connect to GitHub");
+    });
 }
 
 function formSubmitHandler(event) {
